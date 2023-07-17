74f76df021be90306978714406c55abd01049d6e
phuphaGod <105551327+phuphaGod@users.noreply.github.com>
2023-07-17 11:35:11 +0700

    Add files via upload

---
+++ Exercise 2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{  cout << "***Homework 2***" << endl;
+	string name;
+	float Sa;
+	float s;
+	float con;
+	cout << "Enter Name : ";
+	cin >> name;
+	cout << "Enter Salary : ";
+	cin >> Sa;
+	cout << "Enter Sale : ";
+	cin >> s;
+	cout << "Enter Commission Percent : ";
+	cin >> con;
+	cout << "---output---" << endl;
+	cout << "Your name = " << name << endl;
+	cout << "Total Revenue " << Sa+(s*(con/100)) <<" Bath" << endl;
+	system("pause");
+	return (0);
+}
