62cabc4d9727fadc32b9784018b6b6d8c019680f
phuphaGod <105551327+phuphaGod@users.noreply.github.com>
2023-07-22 23:34:03 +0700

    Add files via upload

---
+++ Homework3.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ int Amo;
+ int M1000,M100,M50,M20;
+ cout << "Amount to be withdraw : " ;
+ cin >> Amo ;
+ M1000 = Amo / 1000;
+ M100 = (Amo % 1000)/100 ;
+ M50 = (Amo %100)/50 ;
+ M20 = (Amo %50)/20 ;
+ cout << "1000 : " <<M1000 <<endl;
+ cout << "100 : " <<M100 <<endl;
+ cout << "50 : " <<M50 <<endl;
+ cout << "20 : " <<M20 <<endl;
+return(0);
+}
