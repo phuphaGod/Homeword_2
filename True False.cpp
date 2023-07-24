9117ce79577a47dd45d9675bf61ea262c655afe0
phuphaGod <105551327+phuphaGod@users.noreply.github.com>
2023-07-24 10:03:42 +0700

    Add files via upload

---
+++ True False.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+	int C;
+	cout <<"Input C++ Score : ";
+	cin >> C;
+	cout <<" Your" << ((C>=50 )?" pass":"fali ")<<" with score " << C <<endl;
+
+	system("pause");
+	return(0);
+}
