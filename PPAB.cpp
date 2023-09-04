b3b859f194009b2177da42d6db3dc6509d28e62b
phuphaGod <105551327+phuphaGod@users.noreply.github.com>
2023-09-04 10:45:17 +0700

    Add files via upload

---
+++ PPAB.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+void CalCircle();
+void CalRec();
+
+int main()
+{
+	char Ch;
+	do{
+	    cout<<"Program Calcucalate of Circle."<<endl;
+		cout<<"******************************"<<endl;
+		cout<<"1.Circle"<<endl;
+		cout<<"2.Rectangle"<<endl;
+		cout<<"3.Exit"<<endl;
+		cout<<"Choose Menu : ";
+		cin >> Ch;
+		if(Ch =='1') CalCircle();
+		else if(Ch =='2') CalRec();
+		else if(Ch =='3') cout<<"Exit"<<endl;
+		else cout<<"Wrong Menu"<<endl;
+	}while(Ch != '3');
+	    
+	    system ("pause");
+		return(0);
+}
+
+void CalCircle()
+{
+	float Radius,Area,Circumference;
+	cout <<"Input radius : ";
+	cin >> Radius;
+	Area = 3.14f*Radius*Radius;
+	Circumference = 2*3.14f*Radius;
+	cout <<endl;
+	cout <<"Area of circle : "<<Area<<endl;
+	cout <<"Circumference of circle : "<<Circumference<<endl;
+	cout <<"Diameter of circle : "<<(2*Radius)<<endl;
+}
+
+void CalRec()
+{
+	float wide,high,longer;
+	cout <<"Input wide : ";
+	cin >>wide;
+	cout <<"Input longer : ";
+	cin >>longer;
+	high = wide*longer;
+	cout <<"Area of CalRec : "<<high<<endl;
+}
