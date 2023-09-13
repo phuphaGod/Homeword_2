11a1e9bae6d6c5765009b016f029733e830d1c15
phuphaGod <105551327+phuphaGod@users.noreply.github.com>
2023-09-13 23:36:23 +0700

    Update PPAB.cpp

--- PPAB.cpp
+++ PPAB.cpp
@@ -1,49 +1,40 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
-void CalCircle();
-void CalRec();
 
-int main()
-{
-	char Ch;
-	do{
-	    cout<<"Program Calcucalate of Circle."<<endl;
-		cout<<"******************************"<<endl;
-		cout<<"1.Circle"<<endl;
-		cout<<"2.Rectangle"<<endl;
-		cout<<"3.Exit"<<endl;
-		cout<<"Choose Menu : ";
-		cin >> Ch;
-		if(Ch =='1') CalCircle();
-		else if(Ch =='2') CalRec();
-		else if(Ch =='3') cout<<"Exit"<<endl;
-		else cout<<"Wrong Menu"<<endl;
-	}while(Ch != '3');
-	    
-	    system ("pause");
-		return(0);
+int checkNum(int secret, int guess) {
+    if (guess < secret) {
+        cout << "The secret number is higher" << endl;
+    } else if (guess > secret) {
+        cout << "The secret number is lower" << endl;
+    } else {
+        cout << "Congratulations!" << endl;
+    }
+    return guess;
 }
 
-void CalCircle()
-{
-	float Radius,Area,Circumference;
-	cout <<"Input radius : ";
-	cin >> Radius;
-	Area = 3.14f*Radius*Radius;
-	Circumference = 2*3.14f*Radius;
-	cout <<endl;
-	cout <<"Area of circle : "<<Area<<endl;
-	cout <<"Circumference of circle : "<<Circumference<<endl;
-	cout <<"Diameter of circle : "<<(2*Radius)<<endl;
-}
+int main() {
+    cout << "### Welcome to guessing number game ###" << endl;
+    srand(time(0)); 
+    int secret_number = rand() % 10 + 1; 
+    cout << "Secret number has been chosen" << endl;
 
-void CalRec()
-{
-	float wide,high,longer;
-	cout <<"Input wide : ";
-	cin >>wide;
-	cout <<"Input longer : ";
-	cin >>longer;
-	high = wide*longer;
-	cout <<"Area of CalRec : "<<high<<endl;
+    int guess;
+    int guess_count = 0;
+
+    do {
+        cout << "Guess the number (1 to 10): ";
+        cin >> guess;
+        guess_count++;
+
+        guess = checkNum(secret_number, guess);
+
+    } while (guess != secret_number);
+
+    cout << "The secret number is " << secret_number << endl;
+    cout << "You made " << guess_count << " guesses" << endl;
+
+    return 0;
 }
